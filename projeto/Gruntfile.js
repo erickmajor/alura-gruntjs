d22e9e2d80478d9c8b1d250f34ed8267c7c81410
Erick Major <erick.major@yahoo.com.br>
2018-05-25 00:14:04 -0300

    005: Configurando a task responsável pelo plugin grunt-contrib-uglify.

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -21,4 +21,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');    
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 };
