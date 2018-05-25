c9d8828db3ba2d8fe3ec0f345aeadb2a93082930
Erick Major <erick.major@yahoo.com.br>
2018-05-25 00:12:46 -0300

    004: Configurando a task responsável pelo plugin grunt-contrib-concat.

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -20,4 +20,5 @@ module.exports = function(grunt) {
     
   grunt.loadNpmTasks('grunt-contrib-copy');    
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 };
