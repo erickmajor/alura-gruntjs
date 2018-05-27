00ee8d058dd9bc8252c5c3eca5e114e89c44fce1
Erick Major <erick.major@yahoo.com.br>
2018-05-27 16:00:46 -0300

    Revert "#010: Registrando a task grunt-usemin, responsável por coordenar as tarefas de minificação."
    This reverts commit 61731f4fccf1eda8971366c9ccdc8dbd63d68980.

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -22,12 +22,11 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('default', ['dist']);
 
-    // carregando tasks
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-copy');    
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-usemin');
 };
